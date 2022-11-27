1130bb44ed2545b956e6e661eec3ad77562e8e6b
Kane Chan <kanes.chan@gmail.com>
2022-11-27 12:04:32 -0300

    Listar cursos (api http get)

---
+++ src/main/java/com/kanechan/crudspring/controller/CourseController.java
@@ -0,0 +1,27 @@
+package com.kanechan.crudspring.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kanechan.crudspring.model.Course;
+import com.kanechan.crudspring.repository.CourseRepository;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/api/courses")
+@AllArgsConstructor
+public class CourseController {
+
+    private final CourseRepository courseRepository;
+
+    //@RequestMapping(method = RequestMethod.GET)
+    @GetMapping
+    public List<Course> list() {
+        return courseRepository.findAll();
+    }
+
+}
