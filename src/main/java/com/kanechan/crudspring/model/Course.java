ca2c83eae1fb1d3702bcc8e21e491d43809cc4c1
Kane Chan <kanes.chan@gmail.com>
2022-11-28 08:47:03 -0300

    BD H2 conectando o Angular na API Spring.

--- src/main/java/com/kanechan/crudspring/model/Course.java
+++ src/main/java/com/kanechan/crudspring/model/Course.java
@@ -3,6 +3,8 @@ package com.kanechan.crudspring.model;
 
 
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -16,6 +18,7 @@ public class Course {
     
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @JsonProperty("_id")
     private Long id;
 
     @Column(length = 200, nullable = false)
